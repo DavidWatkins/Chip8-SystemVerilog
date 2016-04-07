260a661a7c625be088986997ee5c417edda4ca01
lpo1234 <lpo1017@frontiernet.net>
2016-04-07 13:29:00 -0400

    Made instruction decode combinational. I believe it works, but not all instrs implemented were tested.

--- Chip8-qsys/Chip8_CPU.sv
+++ Chip8-qsys/Chip8_CPU.sv
@@ -1,6 +1,6 @@
 
 
-module Chip8-CPU(	input logic cpu_clk,
+module Chip8_CPU(	input logic cpu_clk,
 						input logic[15:0] instruction,
 						input logic[3:0] testIn1, testIn2,
 						output logic[7:0] testOut1, testOut2);
@@ -20,26 +20,42 @@ module Chip8-CPU(	input logic cpu_clk,
 			reg_WE1, reg_WE2, reg_WEVF, reg_addr1, reg_addr2,
 			reg_readdata1, reg_readdata2, reg_VFreaddata);
 		
-		always_ff @(posedge cpu_clk) begin
-			if((instruction[15:12] == 4'd6)) begin
+		//always_ff @(posedge cpu_clk) begin
+		always_comb begin
+			alu_in1 = 16'b0;
+			alu_in2 = 16'b0;
+			alu_cmd = 4'b0;
+			alu_carry = 1'b0;
+			reg_writedata1 = 8'b0;
+			reg_writedata2 = 8'b0;
+			reg_VFwritedata = 8'b0;
+			reg_WE1 = 1'b0;
+			reg_WE2 = 1'b0;
+			reg_WEVF = 1'b0;
+			reg_addr1 = 4'b0;
+			reg_addr2 = 4'b0;
+			testOut1 = 8'b0;
+			testOut2 = 8'b0;
+			
+			if((instruction[15:12]) == (4'd6)) begin
 			//6ykk : Vy = kk
-				reg_addr1 <= instruction[11:8];
-				reg_writedata1 <= instruction[7:0];
-				reg_WE1 <= 1'b1;
+				reg_addr1 = instruction[11:8];
+				reg_writedata1 = instruction[7:0];
+				reg_WE1 = 1'b1;
 			end else if((instruction[15:12] == 4'd8) & (~|instruction[3:0])) begin
 			//8xy0 : Vx = Vy
-				reg_addr1 <= instruction[11:8]; //Vx
-				reg_addr2 <= instruction[ 7:4]; //Vy
-				reg_writedata1 <= reg_readdata2;
-				reg_WE1 <= 1'b1; 
+				reg_addr1 = instruction[11:8]; //Vx
+				reg_addr2 = instruction[ 7:4]; //Vy
+				reg_writedata1 = reg_readdata2;
+				reg_WE1 = 1'b1; 
 			end else begin
-				reg_WE1 <= 1'b0;
-				reg_WE2 <= 1'b0;
-				reg_WEVF <= 1'b0;
-				reg_addr1 <= testIn1;
-				reg_addr2 <= testIn2;
-				testOut1 <= reg_readdata1;
-				testOut2 <= reg_readdata2;
+				reg_WE1 = 1'b0;
+				reg_WE2 = 1'b0;
+				reg_WEVF = 1'b0;
+				reg_addr1 = testIn1;
+				reg_addr2 = testIn2;
+				testOut1 = reg_readdata1;
+				testOut2 = reg_readdata2;
 			end
 				
 				
