a744ba2ec0ca570e06cecc2770aedd176d044d2d
Gabrielle A Taylor <gat2118@columbia.edu>
2016-04-10 13:30:36 -0400

    Fix compilation errors
    Simulated in modelsim but still does not work. Loop in line 13 only runs once. Will investigate why.

--- Chip8-qsys/bcd.sv
+++ Chip8-qsys/bcd.sv
@@ -1,30 +1,29 @@
 module bcd(
-	input logic [7:0] num;
-	output logic [3:0] hundreds;
-	output logic [3:0] tens;
-	output logic [3:0] ones;
+	input logic [7:0] num,
+	output logic [3:0] hundreds,
+	output logic [3:0] tens,
+	output logic [3:0] ones);
 	
 	logic [19:0] shift;
-	integer i; 	//loop counter	
 
-	shift[19:8] = 0;
-	shift[7:0] = number;
+	always @ (num) begin
+		shift[19:8] = 11'd0;
+		shift[7:0] = num;
 
-	always_comb begin
-		for (i=0; i<8; i=i+1) begin
-			if(shift[11:8] >= 5)
-				shift[11:8] = shift[11:8] + 3;
-			if(shift[15:12] >= 5)
-				shift[15:12] = shift[15:12] + 3;
-			if(shift[19:16] >= 5)
-				shift[19:16] = shift[19:16] + 3;
+		repeat (8) begin
+			if(shift[11:8] >= 3'd5)
+				shift[11:8] = shift[11:8] + 3'd3;
+			if(shift[15:12] >= 3'd5)
+				shift[15:12] = shift[15:12] + 3'd3;
+			if(shift[19:16] >= 3'd5)
+				shift[19:16] = shift[19:16] + 3'd3;
 			// Shift entire register left once
 			shift = shift << 1;
-
-			hundreds = shift[19:16];
-			tens     = shift[15:12];
-			ones     = shift[11:8];
 		end
+		hundreds = shift[19:16];
+		tens     = shift[15:12];
+		ones     = shift[11:8];
 	end
+	
 
 endmodule
