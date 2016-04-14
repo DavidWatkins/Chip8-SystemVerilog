77d791dc6a4f63a5a0ea4fecd13c111f1d2d1700
David Watkins <djw2146@columbia.edu>
2016-04-13 21:31:10 -0400

    Added in progress version of chip8 top level

--- Chip8-qsys/Chip8_Top.sv
+++ Chip8-qsys/Chip8_Top.sv
@@ -4,6 +4,10 @@
  *
  * Columbia University
  */
+
+// // enumeration type for the CPU state
+// typedef enum 
+// { CPU_f_RUNNING , CPU_f_PAUSED , CPU_f_LOADING , CPU_f_LOADFONT} CPU_f ;
  
 function reg inbetween(input [17:0] low, value, high); 
 begin
@@ -26,7 +30,6 @@ module Chip8_Top(
 
 	logic [15:0] i, pc, sp;
 	logic [511:0] stack;
-	logic [32767:0] memory;
 
 	//Framebuffer values
 	logic fbreset;
@@ -42,105 +45,119 @@ module Chip8_Top(
 	logic ispressed;
 	logic [3:0] key;
 	
-	//Register File
-	logic[7:0] 	regwritedata1, regwritedata2, VFwritedata;
-	logic 		regWE1, regWE2, regWEVF;
-	logic[3:0]	regaddr1, regaddr2;
-	logic[7:0]	regreaddata1, regreaddata2, regVFreaddata;
+	//Memory
+	logic [7:0] memwritedata1, memwritedata2;
+	logic memWE1, memWE2;
+	logic [11:0] memaddr1, memaddr2;
+	logic [7:0] memreaddata1, memreaddata2;
 
-	always_ff @(posedge clk)
+	//CPU
+	logic [15:0] cpu_instruction;
+	logic [3:0] cpu_testIn1, cpu_testIn2;
+	logic [7:0] cpu_testOut1, cpu_testOut2;
+
+	always_ff @(posedge clk) begin : proc_
 		if(reset) begin
-			fbreset <= 1'b1;
-			memory <= 32768'd0;
-		end else if(write && chipselect) begin
+			 //Add initial values for code
+			 pc <= 15'h200;
+
+		//Handle input from the ARM processor
+		end else if(chipselect) begin
 			if(address[16]) begin
-				memory[0] <= 1'b0; //Fix me
+				memaddr1 <= address[11:0];
+				data_out <= memreaddata1;
+				if(write) begin
+					memWE1 <= 1'b1;
+					memwritedata1 <= writedata[7:0];
+				end
 			end else begin
 				case (address) 
 				
-					inbetween(17'h0, address, 17'hF) : begin
-						regwritedata1 <= writedata[7:0];
-						regWE1 <= 1'b1;
-						//regaddr1 <= address[3:0];
-					 end
-
-					17'h10 : i <= writedata[15:0];
+					// inbetween(17'h0, address, 17'hF) : begin end
 					// 17'h11 : data_out <= {32'b0}; //Fix me
 					// 17'h12 : data_out <= {32'b0}; //Fix me
-					17'h13 : sp <= writedata[5:0]; //0-63
-					17'h14 : pc <= writedata[11:0]; //0-4095
 
-					17'h15 : begin
-								ispressed <= writedata[7:4];
-								key <= writedata[3:0];
-							end
-
-					17'h16 : state <= writedata[1:0];
-
-					17'h17 : begin 
-								fbvx <= writedata[11:8]; 
-								fbvy <= writedata[7:4];
-								fbdata <= writedata[3:0];
-								fbwrite <= 1'b1;
-								end
-
-					17'h18 : stack[0] <= 1'b0; //Fix me
-
-				endcase
-			end
-		end else begin
-			fbwrite <= 1'b0;
-			regWE1 <= 1'b0;
-			regWE2 <= 1'b0;
-			regWEVF <= 1'b0;
-		end
-		
-	// OUTPUTS
-	// deltas for joint parameters
-	always_ff @(posedge clk) begin
-		if (reset) begin
-			data_out <= {32'b0};
-		end else if ( chipselect ) begin
-			if(address[16]) begin
-				data_out <= {32'b0}; //Fix me
-			end else begin
-				case (address)
-
-					inbetween(17'h0, address, 17'hF) : begin
-						regaddr1 <= address[3:0];
-						data_out <= {24'b0, regreaddata1};
+					17'h10 : begin
+						i <= writedata[15:0];
+						data_out <= {16'b0, i};
 					end
-
-					17'h10 : data_out <= {16'b0, i};
 					17'h11 : data_out <= {32'b0}; //Fix me
 					17'h12 : data_out <= {32'b0}; //Fix me
-					17'h13 : data_out <= {16'b0, sp};
-					17'h14 : data_out <= {16'b0, pc};
-					17'h15 : data_out <= {32'b0}; //No reading keypress
-					17'h16 : data_out <= {30'b0, state};
-					17'h17 : data_out <= {32'b0}; //Read Framebuffer?
-					17'h18 : data_out <= {32'b0}; //Fix me {16'b0, stack[sp]}
+					
+					17'h13 : begin
+						data_out <= {16'b0, sp};
+						if(write) sp <= writedata[5:0]; //0-63
+					end
+
+					17'h14 : begin 
+						data_out <= {16'b0, pc};
+						if(write) pc <= writedata[11:0]; //0-4095
+					end
+
+					17'h15 : begin 
+						data_out <= {32'b0}; //No reading keypress
+						if(write) begin
+							ispressed <= writedata[7:4];
+							key <= writedata[3:0];
+						end
+					end
+
+					17'h16 : begin
+						data_out <= {30'b0, state};
+						if(write) state <= writedata[1:0];
+					end
+
+					17'h17 : begin
+						data_out <= {32'b0}; //Read Framebuffer?
+						if(write) begin 
+							fbvx <= writedata[11:8]; 
+							fbvy <= writedata[7:4];
+							fbdata <= writedata[3:0];
+							fbwrite <= 1'b1;
+						end
+					end
+
+					17'h18 : begin 
+						data_out <= {32'b0}; //Fix me {16'b0, stack[sp]}
+						if(write) stack[0] <= 1'b0; //Fix me
+					end	
 
 				endcase
 			end
+
+		//Normal processor continuation if State is running
+		end else if(state == 2'h0) begin
+			pc <= pc + 2;
 		end
+	end
+
+	always_comb begin 
+		memaddr1 = pc[11:0];
+		memaddr2 = pc[11:0] + 1;
+
+		cpu_instruction = (memreaddata1 << 8) | (memreaddata2);
 	end
 
 	Framebuffer framebuffer(.clk(clk), 
 							.reset(fbreset), 
 							.write(fbwrite),
 							.*);	
-							
-	Chip8_register_file reg_file (.cpu_clk(clk), 
-							 .WE1 	(regWE1), 
-							 .WE2 	(regWE2),
-							 .WEVF 	(regWEVF), 
-							 .writedata1 (regwritedata1),
-							 .writedata2 (regwritedata2),
-							 .VFwritedata (VFWritedata),
-							 .addr1 (regaddr1),
-							 .addr2 (regaddr2),
-							 .readdata1 (regreaddata1),
-							 .readdata2 (regreaddata2),
-							 .VFreaddata (regVFreaddata));
+
+	Chip8_memory memory(.cpu_clk(clk),
+					.writedata1(memwritedata1),
+					.writedata2(memwritedata2),
+					.WE1(memWE1),
+					.WE2(memWE2),
+					.addr1(memaddr1),
+					.addr2(memaddr2),
+					.readdata1(memreaddata1),
+					.readdata2(memreaddata2));
+
+	Chip8_CPU cpu(	.cpu_clk(clk),
+					.instruction(cpu_instruction),
+					.testIn1(cpu_testIn1),
+					.testIn2(cpu_testIn2),
+					.testOut1(cpu_testOut1),
+					.testOut2(cpu_testOut2));
+
 endmodule
