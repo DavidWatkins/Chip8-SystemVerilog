e9f73189e4d6b106bb55e6d1d9112f23d9938ed0
lpo1234 <lpo1017@frontiernet.net>
2016-05-09 20:19:20 -0400

    Possible draw-sprite fix.

--- Chip8-qsys/Chip8_CPU/Chip8_CPU.sv
+++ Chip8-qsys/Chip8_CPU/Chip8_CPU.sv
@@ -83,8 +83,7 @@ module Chip8_CPU(
 	logic[7:0] to_bcd;
 	wire[3:0] bcd_hundreds, bcd_tens, bcd_ones;
 	
-	wire[31:0] stage_shift_hold = (stage >> 32'h4) - 32'h1;
-	wire[7:0] stage_shifted_by4_minus1 = stage_shift_hold[7:0];
+	wire[31:0] stage_shifted_by4_minus1 = (stage >> 32'h4) - 32'h1;
 	logic[3:0] num_rows_written; //used for sprite writing
 
 	Chip8_rand_num_generator rand_num_generator(cpu_clk, rand_num);
@@ -536,8 +535,8 @@ module Chip8_CPU(
 					else num_rows_written = stage_shifted_by4_minus1[3:0];//((stage >> 32'h4) - 32'h1);
 					
 					mem_addr1 = reg_I_readdata[11:0] + {8'b0,num_rows_written};
-					mem_request = (stage >= 32'd16) & (num_rows_written < instruction[3:0]) & !(stage[0]);
-					fb_WE = (stage >= 32'd16) & (num_rows_written < instruction[3:0]) & (stage[0]);
+					mem_request = (stage >= 32'd16) & (stage_shifted_by4_minus1 < instruction[3:0]) & !(stage[0]);
+					fb_WE = (stage >= 32'd16) & (stage_shifted_by4_minus1 < instruction[3:0]) & (stage[0]);
 					fb_addr_x = reg_readdata1 + ({5'b0, stage[3:1]});
 					fb_addr_y = reg_readdata2 + ({4'b0, num_rows_written});
 					fb_writedata = mem_readdata1[stage[3:1]] ^ fb_readdata;
